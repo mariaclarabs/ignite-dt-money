7d28fc38f34cc38e68233089eaaaf690afa44e60
mariaclarabs <clarabsouza@hotmail.com>
2021-07-08 18:09:36 -0300

    Assets/styled-components/global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
       <h1>dt money</h1>
+      <GlobalStyle />
     </div>
   );
 }
