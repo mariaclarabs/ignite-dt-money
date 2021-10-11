cb4bd3f196347400fdbd3171602c9b88d41a337a
mariaclarabs <clarabsouza@hotmail.com>
2021-10-11 17:36:28 -0300

    Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>dt money</h1>
+    <>
+      <Header />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
 
